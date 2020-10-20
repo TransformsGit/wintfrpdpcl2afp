506c6e1bb1cc166257649c32bbc658eb09315d8c
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-20 17:05:48 +0300

    Add files via upload

--- 4create_folder_structure_PCL2AFP_win.bat
+++ 4create_folder_structure_PCL2AFP_win.bat
@@ -15,12 +15,12 @@ echo mkdir E:\OutputTransformPCL2AFP\win\output\out_AFP\TestFiles
 mkdir E:\OutputTransformPCL2AFP\win\output\out_PDF\TestFiles
 echo mkdir E:\OutputTransformPCL2AFP\win\output\out_PDF\TestFiles
 
-mkdir E:\OutputTransformPCL2AFP\win\output\out_TIFF\MF
-echo mkdir E:\OutputTransformPCL2AFP\win\output\out_TIFF\MF
+mkdir E:\OutputTransformPCL2AFP\win\output\out_TIFF\MasterFiles
+echo mkdir E:\OutputTransformPCL2AFP\win\output\out_TIFF\MasterFiles
 
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPCL2AFP\win\output\out_AFP\TestFiles\ /s /i
 c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPCL2AFP\win\output\out_PDF\TestFiles\ /s /i
-c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPCL2AFP\win\output\out_TIFF\MF\ /s /i
+c:\Windows\System32\xcopy.exe /T /e e:\TestFiles_folder_structure\TestFiles\* E:\OutputTransformPCL2AFP\win\output\out_TIFF\MasterFiles\ /s /i
 
 
 echo Folder structure succesfully done!
